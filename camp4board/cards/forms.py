e8bda54da8cf515227b06e3082ba6433d987daf9
Matthew Carroll <60335800+mmc102@users.noreply.github.com>
2023-10-10 14:51:49 -0700

    add expire to form

--- camp4board/cards/forms.py
+++ camp4board/cards/forms.py
@@ -4,5 +4,5 @@ from .models import Card
 class CardForm(forms.ModelForm):
     class Meta:
         model = Card
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'expiration_date']
 
