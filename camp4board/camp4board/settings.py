cf7b1e342872e54fe315b8b198181e0db0d95591
Matthew Carroll <60335800+mmc102@users.noreply.github.com>
2023-10-10 10:15:11 -0700

    env vars for passwords

--- camp4board/camp4board/settings.py
+++ camp4board/camp4board/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,20 @@ WSGI_APPLICATION = "camp4board.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+database_name = os.environ['DB_NAME']
+database_user = os.environ['DB_USER']
+database_passWORD = os.environ['DB_PASSWORD']
+database_host = os.environ['DB_HOST']
+database_port = os.environ['DB_PORT']
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': database_name,
+        'USER': database_user,
+        'PASSWORD': database_password,
+        'HOST': database_host,
+        'PORT': database_port,
     }
 }
 
