250799ad2dd453d539e4e69f0e2c6d8052c0e6f2
Matthew Carroll <60335800+mmc102@users.noreply.github.com>
2023-10-10 14:34:55 -0700

    more

--- camp4board/cards/views.py
+++ camp4board/cards/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect
 from .models import Card
 from .forms import CardForm
 from django.utils import timezone
+from django.utils import timezone
+from django.shortcuts import render
+from .models import Card
 
 def card_list(request):
-    non_expired_cards = Card.objects.all()
+    now = timezone.now()
+    non_expired_cards = Card.objects.filter(expiration_date__gte=now)
     return render(request, 'cards/card_list.html', {'cards': non_expired_cards})
 
 def add_card(request):
