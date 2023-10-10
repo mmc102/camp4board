12c81331a2b8e5efee9b787149a49bf87b35e8d4
Matthew Carroll <60335800+mmc102@users.noreply.github.com>
2023-10-10 13:01:34 -0700

    attempt

--- camp4board/camp4board/settings.py
+++ camp4board/camp4board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ["18.221.78.251","camp4board.com"]
+ALLOWED_HOSTS = ["18.221.78.251"]
 
 # Application definition
 
