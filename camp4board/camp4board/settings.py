2889056d238f2c420ab1efd6137bf3225086cc52
Matthew Carroll <60335800+mmc102@users.noreply.github.com>
2023-10-10 11:54:47 -0700

    remove key

--- camp4board/camp4board/settings.py
+++ camp4board/camp4board/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--e0+!8-_tvn*9f9cuw5_%##-qh67fioknas@*h!-^k8w$r21t$"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
