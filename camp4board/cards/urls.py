219bfc5c0b21ce2b835357b62de83eed6a26f8cb
Matthew Carroll <60335800+mmc102@users.noreply.github.com>
2023-10-10 14:48:52 -0700

    fix

--- camp4board/cards/urls.py
+++ camp4board/cards/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.card_list, name='card_list'),
+    path('', views.card_list, name='card_list'),
     path('add_card/', views.add_card, name='add_card'),
 ]
