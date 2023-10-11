ed8942505cbd387cdd0c9aa7f540426e90fd9c64
Matthew Carroll <60335800+mmc102@users.noreply.github.com>
2023-10-11 12:34:29 -0700

    unify gunicorn

--- camp4board/gunicorn_config.py
+++ camp4board/gunicorn_config.py
@@ -1,9 +1,31 @@
 import multiprocessing
 
-bind = "127.0.0.1:8000"
+bind = "localhost:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gthread"
 threads = 2
 
-errorlog = "error.txt"
-accesslog = "access.txt"
+# gunicorn_config.py
+import logging
+
+logconfig_dict = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',  # Set the desired log level, e.g., 'DEBUG' for maximum logging.
+    },
+}
+logconfig_dict['loggers'] = {
+    'gunicorn.error': {
+        'level': 'DEBUG',  # Adjust the log level as needed.
+        'handlers': ['console'],
+        'propagate': False,
+        'qualname': 'gunicorn.error',
+    },
+}
